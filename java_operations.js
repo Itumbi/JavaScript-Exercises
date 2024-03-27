74bc5a405b8f3ce2fb162312a10a319e06d33c10
itumbi <imanase37@gmail.com>
2024-03-27 13:08:48 -0700

    My first JavaScript commit

---
+++ java_operations.js
@@ -0,0 +1,39 @@
+//Assigning numbers in arithmetric operations in JavaScript for learning purpose
+var x=90;
+var y=40;
+//Addition +
+console.log(x+y);
+//Subtraction -
+console.log(x-y);
+//Multiplication *
+console.log(x*y);
+//Division /
+console.log(x/y);
+//Remainder //
+console.log(x%y);
+
+//Performing logic operations
+//let x=90;
+//let y=40; Already defines
+// Using AND operation &&
+console.log(x<y && y>x)
+//Using OR operation ||
+console.log(x>y || x<y)
+//Using NOT operation !
+console.log(!false)
+
+//performing assignment operation
+//let x=90;
+//let y=40;  Already defined
+//Using addition and assignment +=
+console.log(x+=y)
+//Using subtraction and assignment -=
+console.log(x-=y)
+//Using division and assignment /=
+console.log(x/=y)
+//Using multiplication and assignment *=
+console.log(x*=y)
+//Using Equality operators ==
+console.log(x==y)
+//Using strict Equality operators ===
+console.log(x===y)
